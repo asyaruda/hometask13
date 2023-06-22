3a01999a4b0f59d09f5d7bc7c578251693ba7b99
Anastasiia Kholod <asyaruda06@gmail.com>
2023-06-22 19:03:56 +0300

    update

--- twoDimensionalArray.js
+++ twoDimensionalArray.js
@@ -1,22 +1,25 @@
+
 function fill2DArray() {
+  const rows = parseInt(prompt('Enter the number of rows: '))
+  const columns = parseInt(prompt('Enter the number of columns: '))
 
-  let rows = prompt('Enter the number of rows: ')
-  let columns = prompt('Enter the number of columns: ')
-
+  if (isNaN(rows) || isNaN(columns) || !Number.isInteger(rows) || !Number.isInteger(columns) || rows <= 0 || columns <= 0) {
+    return 'Error: Invalid input'
+  }
+  
+  const array = []
  
-  let array = [];
-
   for (let i = 0; i < rows; i++) {
-    let row = []
+    const row = []
     for (let j = 0; j < columns; j++) {
-      let value = prompt('Enter a value for the item [' + i + ', ' + j + ']: ')
+      const value = prompt('Enter a value for the item [' + i + ', ' + j + ']: ')
       row.push(value)
     }
     array.push(row)
   }
-
+  
   return array
 }
-
-let data = fill2DArray()
+  
+const data = fill2DArray()
 console.log(data)
